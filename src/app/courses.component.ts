b34f226c16af7931154737b19c0de56147f112bf
Pratik Singh <spratik@vmware.com>
2018-05-27 20:34:56 -0700

    added list

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// decorator function
+@Component({
+  selector: 'courses',
+  template: '<h1>Courses</h1>'
+})
+
+export class CoursesComponent {
+
+
+}
