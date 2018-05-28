ad893ea3b24bdf8ddd45194c7edc52cd5df7cb1b
Pratik Singh <spratik@vmware.com>
2018-05-27 21:09:30 -0700

    added courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, CoursesComponent
+    AppComponent, CoursesComponent, CourseComponent
   ],
   imports: [
     BrowserModule
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,31 @@ import { Component } from '@angular/core';
 // decorator function
 @Component({
   selector: 'courses',
-  template: '<h1>Courses</h1>'
+  template: `
+    <h1>{{ "Courses " + getTitle() }}</h1>
+    <br>
+    <ul>
+      
+      <li *ngFor="let course of getCourses()">
+        {{ course }}
+      </li>
+      
+    </ul>
+  
+  `
 })
 
 export class CoursesComponent {
 
+  title = "Course Title";
+  courses = ['course 1', 'course 2', 'course 3', 'course 4'];
+
+  getTitle() {
+    return this.title;
+  }
+
+  getCourses() {
+    return this.courses;
+  }
 
 }
